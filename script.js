e8b4d1aaf06e8f2e202883daa15d07a7c73ad4cd
kuttate <86170824+kuttate@users.noreply.github.com>
2025-07-23 13:48:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 2;
+  const y = (e.clientY / window.innerHeight - 0.5) * 2;
+
+  const loadborder = document.getElementById("loadborder");
+  const percentage = document.getElementById("percentage");
+
+
+
+  const setParallax = (el, speed) => {
+    el.style.transform = `translate(${x * speed}px, ${y * speed}px)`;
+  };
+
+  setParallax(loadborder, 14);
+  setParallax(percentage, 10);
+
+
+
+
+
+
+});
